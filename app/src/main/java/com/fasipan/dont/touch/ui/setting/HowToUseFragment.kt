77533337b4415936b3455303e109e6a69120ef07
Phạm Ánh Trường <61191253+phamtruong2k1@users.noreply.github.com>
2024-01-04 09:59:30 +0700

    push code 3.0

--- app/src/main/java/com/fasipan/dont/touch/ui/setting/HowToUseFragment.kt
+++ app/src/main/java/com/fasipan/dont/touch/ui/setting/HowToUseFragment.kt
@@ -1,25 +1,25 @@
 package com.fasipan.dont.touch.ui.setting
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.fasipan.dont.touch.R
 import com.fasipan.dont.touch.base.BaseFragment
-import com.fasipan.dont.touch.databinding.FragmentSettingBinding
+import com.fasipan.dont.touch.databinding.FragmentHowToUseBinding
 import com.fasipan.dont.touch.utils.ex.clickSafe
 
-class SettingFragment : BaseFragment() {
+class HowToUseFragment : BaseFragment() {
 
-    private lateinit var binding: FragmentSettingBinding
+    private lateinit var binding: FragmentHowToUseBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        binding = FragmentHowToUseBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -35,8 +35,6 @@ class SettingFragment : BaseFragment() {
 
     private fun initListener() {
         binding.imgBack.clickSafe { onBack() }
-        binding.imgHelp.clickSafe {
-            findNavController().navigate(R.id.action_settingFragment_to_howToUseFragment)
-        }
+
     }
 }
